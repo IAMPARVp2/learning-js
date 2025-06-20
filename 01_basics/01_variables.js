66fdcf4c56659fc7ebe4573ff2d11e6d6d35276c
Parv Jain <95540208+IAMPARVp2@users.noreply.github.com>
2025-06-20 12:34:15 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+// curley braces{} this is the scope in java script
+
+const accountId = 122324 // const - not changeable
+let accountEmail = "jainv1071@gmail.com" 
+var accountPassword = "1222342345"    
+/*
+prefer not to use var because
+of the issue in the block scope and functional scope
+*/
+accountCity = "Jaipur"// prefer not to use this method for declaring variable
+
+// accountId = 2 // not allowed
+let accountState
+accountEmail ="hc@hc.com"
+accountPassword = "21342351"
+accountCity ="benguluru"s
+
+console.log(accountId);
+console.table([accountEmail, accountPassword, accountCity, accountState])
