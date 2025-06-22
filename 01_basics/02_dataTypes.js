7ff2c3cc06517f172ffd5a5e72d24ae4a665e960
Parv Jain <95540208+IAMPARVp2@users.noreply.github.com>
2025-06-22 04:48:10 +0000

    dataTypes

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -1,28 +1,30 @@
-"use strict";  //treat all JS code as newer version ofJS
-//alert (3+3)// we are using nodejs, not browser
+"use strict";  // treat all JS code as newer version ofJS
+
+// alert (3+3)// we are using nodejs, not browser
 // console.log(3+3);   console.log("hitesh");// notb a good practice to write them in one line
+
 console.log(3
     +
-    3);   console.log("hitesh");//ALTHoUGH IT GIVES OUTPUT
+    3);   console.log("hitesh");// ALTHoUGH IT GIVES OUTPUT
 // but its NOT A GOOD PRACTICE as IT GIVES WORST READABILITY
 
-//DATATYPES
+// DATATYPES
 
 let NAME ="PARV";
 let age =18
 let isLoggedIn = false
 let state = null
 
-//number  =>2 to the power 154
-//bigint
+// number  =>2 to the power 154
+// bigints
 // string  =>""
 // boolean=> false/true
-//null =>standalone value
+// null =>standalone value
 // undefined => value not assigned yet
 
-//symbol=> unique
+// symbol=> unique
 
-//object
+// object
 
 console.log(typeof "parv")
 console.log(typeof undefined)
