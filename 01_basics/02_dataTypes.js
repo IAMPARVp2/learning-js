fe4313c65c94b83beeeba2b0d0a892d9621f159c
Parv Jain <95540208+IAMPARVp2@users.noreply.github.com>
2025-06-22 04:44:10 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,37 @@
+"use strict";  //treat all JS code as newer version ofJS
+
+//alert (3+3)// we are using nodejs, not browser
+
+
+// console.log(3+3);   console.log("hitesh");// notb a good practice to write them in one line
+
+
+console.log(3
+    +
+    3);   console.log("hitesh");//ALTHoUGH IT GIVES OUTPUT
+// but its NOT A GOOD PRACTICE as IT GIVES WORST READABILITY
+
+
+//DATATYPES
+
+let NAME ="PARV";
+let age =18
+let isLoggedIn = false
+let state = null
+
+
+
+//number  =>2 to the power 154
+//bigint
+// string  =>""
+// boolean=> false/true
+//null =>standalone value
+// undefined => value not assigned yet
+
+//symbol=> unique
+
+//object
+
+console.log(typeof "parv")
+console.log(typeof undefined)
+console.log(typeof null)
