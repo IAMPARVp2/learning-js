f33b367189a1dd41224972c395eed77ea66c9484
Parv Jain <95540208+IAMPARVp2@users.noreply.github.com>
2025-06-22 16:13:31 +0000

    conversionOperations

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -2,6 +2,7 @@ let score = "33"     // 33
 // let score = "parv"   // 
 // let score = "33abc"  // NaN
 // let score = true       // TRUE =>1 / FALSE => 0
+// let score = undefined  // NaN Not A Number
 
 console.log(typeof score);
 console.log(typeof (score));
@@ -10,3 +11,20 @@ let convInNumber = Number(score)
 console.log(typeof convInNumber);
 console.log(convInNumber);1
 
+
+// let isLoggedIn = 1
+let isLoggedIn;
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+
+// 1=> true; 0=>false
+
+// "" => false
+// "parv" => true
+
+let someNumber =33
+
+let stringNumber =String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
