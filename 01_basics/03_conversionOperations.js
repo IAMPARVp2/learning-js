2531620c4e8579abe8247dddce9ecb7c8e25b605
Parv Jain <95540208+IAMPARVp2@users.noreply.github.com>
2025-06-22 05:13:46 +0000

    conversionOperations

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,12 @@
+let score = "33"     // 33
+// let score = "parv"   // 
+// let score = "33abc"  // NaN
+// let score = true       // TRUE =>1 / FALSE => 0
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let convInNumber = Number(score)
+console.log(typeof convInNumber);
+console.log(convInNumber);1
+
