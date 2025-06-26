067e2047e17e3a69458f490d61030aa8ffb9a97b
Parv Jain <95540208+IAMPARVp2@users.noreply.github.com>
2025-06-26 13:00:28 +0000

    STRING

---
+++ 01_basics/string.js
@@ -0,0 +1,33 @@
+const name = "parv" //declaring the string
+const repoCount =50
+
+console.log(name + repoCount + "value"); //its out dated not mordern we should not use them
+
+
+//new and prefered syntax
+//use backticks- here we use string interpolation
+// and we make placeholders here in which we inject the variables directly
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+//second method to declare the string is given below
+
+// const gameName = new String ("parv-jain")
+const gameName = new String ("parv-jain")
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('p'));
+
+const newString = gameName.substring(0,4)//last value is not included while printing the subString only 0123 indices included
+console.log(newString);
+
+const anotherString = gameName.slice(-8, 3)
+
+console.log(gameName);
+console.log(anotherString);
+
+
